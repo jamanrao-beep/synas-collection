46a7a95d9e3fc135331eaf982724e27df80db338
Aman J <jamanrao@gmail.com>
2026-07-11 00:05:52 +0530

    footer edited

--- components/SiteFooter.tsx
+++ components/SiteFooter.tsx
@@ -90,20 +90,11 @@ export default function SiteFooter() {
                                 </svg>
                             </div>
                             <div className={styles.contactContent}>
-                                <span className={styles.contactLabel}>UNITED STATES</span>
-                                <span className={styles.contactText}>+1 508-507-9623</span>
-                            </div>
-                        </div>
-
-                        <div className={styles.contactItem}>
-                            <div className={styles.iconBox}>
-                                <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round">
-                                    <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-                                </svg>
-                            </div>
-                            <div className={styles.contactContent}>
-                                <span className={styles.contactLabel}>INDIA</span>
-                                <span className={styles.contactText}>+91 89760-29638</span>
+                                <span className={styles.contactLabel}>PHONE</span>
+                                <span className={styles.contactText}>
+                                    US: +1 508-507-9623<br />
+                                    INDIA: +91 89760-29638
+                                </span>
                             </div>
                         </div>
 
